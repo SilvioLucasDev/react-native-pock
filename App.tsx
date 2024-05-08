ba8cec91a82c025d4fc72afca4e379f6304a3fc0
“SilvoLucasDev” <silviolucas_santos@hotmail.com>
2024-05-07 23:02:32 -0300

    chore: install expo fonts and configure

--- App.tsx
+++ App.tsx
@@ -1,10 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
 import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { config } from '@/config/theme/gluestack-ui.config';
 
+import { Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
+
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [fontsLoaded, fontsError] = useFonts({
+    Roboto_700Bold,
+    Roboto_400Regular,
+    RobotoMono_Regular: require('@/assets/fonts/RobotoMono.ttf'),
+  });
+
+  React.useEffect(() => {
+    if (fontsError) throw fontsError;
+  }, [fontsError]);
+
+  React.useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   return (
     <GluestackUIProvider config={config}>
       <View style={styles.container}>
