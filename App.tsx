8d7aafaef13e532aa0c35621e82a808d2b602399
“SilvoLucasDev” <silviolucas_santos@hotmail.com>
2024-05-07 22:54:05 -0300

    chore: install and configure gluestack

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { GluestackUIProvider } from '@gluestack-ui/themed';
+import { config } from '@/config/theme/gluestack-ui.config';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider config={config}>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Text>Init</Text>
+      </View>
+    </GluestackUIProvider>
   );
 }
 
